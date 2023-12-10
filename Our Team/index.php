bdee923e6a7e81323c25bda964f2d40b5ffedd51
Thiraniihara <thiranihara@gmail.com>
2023-12-10 09:43:41 +0530

    Revert "Merge branch 'main' of https://github.com/RedsharkG/Brave_Project"
    This reverts commit b029d9e279eb374fbca34c390ab94b07825b8e9b, reversing
    changes made to 7e9d54713707451f5cf86adf6c5247113a233478.

--- Our Team/index.php
+++ Our Team/index.php
@@ -11,70 +11,80 @@
     <link rel="stylesheet" href="style.css" />
     <link href="../Header/style.css" rel="stylesheet"/>
     <link rel="stylesheet" href="../Footer/style.css" />
+
     <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
 
     <title>Our Team</title>
-
 </head>
 
 <body>
-
 <header>    <?php include '../Header/index.php'; ?>
 </header>
-
-<!--HEAD BORDER-->
 <div class="head-border">
     <div class="head-el-01">our</div>
     <div class="head-el-02"><img class="logo" src="Images/logo.png" /></div>
     <div class="head-el-03">team</div>
 </div>
 
-<!--PARAGRAPH-->
-<div><p class="p-tag2">ALL THE MEMBERS OF OUR TEAM ARE UNDER-GRADUATES OF THE
+<!--PICTURES-->
+<div class="pic-container">
+    <!--  <div class="first-row-names">-->
+    <div class="el--1">sehara<br> gishan</div>
+    <div class="el--2">udaya<br> sandaruwan</div>
+    <div class="el--3">thenuri <br>gamage</div>
+    <!--  </div>-->
+
+    <!--<div class="first-row-imgs"-->
+    <div class="el--4"><img class="profile" src="Images/shehara.jpg"></div>
+    <div class="el--5"><img class="profile" src="Images/udaya.jpg"></div>
+    <div class="el--6"><img class="profile" src="Images/thenuri.jpg"></div>
+    <!--    </div>-->
+
+    <!--    <div class="sec-row-names">-->
+    <div class="el--7">lasan<br> sanjula</div>
+    <div class="el--8">yuhas <br>himsanda</div>
+    <div class="el--9">thirani <br>ihara</div>
+    <!--      </div>-->
+
+    <!--    <div class="sec-row-imgs">-->
+    <div class="el--10"><img class="profile" src="Images/lasan.jpg"></div>
+    <div class="el--11"><img class="profile" src="Images/yuhas.jpg"></div>
+    <div class="el--12"><img class="profile" src="Images/thirani.jpg"></div>
+    <!--      </div>-->
+
+    <!--    <div class="third-row-names">-->
+    <div class="el--13">piyumaka <br>wijeyaratne</div>
+    <div class="el--14">dhanithe<br> subasinghe</div>
+    <div class="el--15">yohani <br>hansika</div>
+    <!--    </div>-->
+
+    <!--    <div class="third-row-imgs">-->
+    <div class="el--16"><img class="profile" src="Images/piyumaka.jpg"></div>
+    <div class="el--17"><img class="profile" src="Images/danith.jpg"></div>
+    <div class="el--18"><img class="profile" src="Images/yohani"></div>
+    <!--    </div>-->
+
+    <!--    <div class="last-row-name">-->
+    <div></div>
+    <div class="el--19">nuran <br>hirusha</div>
+    <div></div>
+    <!--    </div>-->
+
+    <!--<div class="last-row-img">-->
+    <div></div>
+    <div class="el--20"><img class="profile" src="Images/nuran.jpg"></div>
+    <div></div>
+    <!--</div>-->
+</div>
+<div class="p-tag2"><p>ALL THE MEMBERS OF OUR TEAM ARE UNDER-GRADUATES OF THE
         NSBM GREEN UNIVERSITY AND WE HAVE CREaTED THIS WEBSITE FOR OUR WEB DEVELOPING
         FINAL GROUP PROJECT. SPECIAL THANKS FOR OUR LECTURER AND THANKS FOR THE
         ALL OF GROUP MEMBERS IN OUR GROUP. WE ARE PROVIDING YOU THE BEST QUALITY
         T-SHIRT FROM OUR BRAVE STORE. <br> THANK YOU!</p>
 </div>
-
-<!--PICTURES-->
-<div class="pic-container">
-    <div class="el--1">sehara<br> gishan</div>
-    <div class="el--2">udaya<br> sandaruwan</div>
-    <div class="el--3">thenuri <br>gamage</div>
-
-    <div class="el--4"><img class="profile" src="Images/shehara.jpg"></div>
-    <div class="el--5"><img class="profile" src="Images/udaya.jpg"></div>
-    <div class="el--6"><img class="profile" src="Images/thenuri.jpg"></div>
-
-    <div class="el--7">lasan<br> sanjula</div>
-    <div class="el--8">yuhas <br>himsanda</div>
-    <div class="el--9">thirani <br>ihara</div>
-
-    <div class="el--10"><img class="profile" src="Images/lasan.jpg"></div>
-    <div class="el--11"><img class="profile" src="Images/yuhas.jpg"></div>
-    <div class="el--12"><img class="profile" src="Images/thirani.jpg"></div>
-
-    <div class="el--13">piyumaka <br>wijeyaratne</div>
-    <div class="el--14">dhanithe<br> subasinghe</div>
-    <div class="el--15">yohani <br>hansika</div>
-
-    <div class="el--16"><img class="profile" src="Images/piyumaka.jpg"></div>
-    <div class="el--17"><img class="profile" src="Images/danith.jpg"></div>
-    <div class="el--18"><img class="profile" src="Images/yohani"></div>
-
-    <div></div>
-    <div class="el--19">nuran <br>hirusha</div>
-    <div></div>
-
-    <div></div>
-    <div class="el--20"><img class="profile" src="Images/nuran.jpg"></div>
-    <div></div>
-</div>
-
 
 <footer>
     <?php include '../Footer/index.php'; ?>
